e91e793d46d62b7744be963ca139a747c436fcb8
nathanccarnelos <nathancoltinho@gmail.com>
2020-08-27 23:02:11 -0300

    alot of changes

--- content.js
+++ content.js
@@ -1,31 +1,54 @@
+const wppHeaderClass = "._1QUKR" ;
+
+const storageSpeed = {
+    get: () => Number(localStorage.getItem('wpp-speed')),
+    set: (v) => localStorage.setItem('wpp-speed', v)
+};
+
+if(!storageSpeed.get()) {
+    storageSpeed.set(1)
+};
+
 const interval = setInterval(() => {
-    const header = document.querySelector("._1QUKR")
-    if (header) {
-        clearInterval(interval);
-        const button = document.createElement("button")
-        let speed = 1
-        button.innerHTML = speed + 'x';
-        button.classList.add("twoTimesButton");
-        header.appendChild(button);
+    const header = document.querySelector(wppHeaderClass);
+    if (!header) return;
 
-        button.addEventListener("click", () => {
-            if (speed === 2) {
-                speed = 1
-                button.classList.remove("twoTimesButtonActive");
-                button.innerHTML = speed + 'x';
-                return
-            }
-            speed += 0.25
-            button.classList.add("twoTimesButtonActive");
-            button.innerHTML = speed + 'x';
+    clearInterval(interval);
+    let speed = storageSpeed.get();
+    const button = createButton(speed)
+    header.appendChild(button);
+
+    button.addEventListener("click", () => {
+        if (speed === 2) {
+            speed = 1;
+            storageSpeed.set(speed);
+            button.classList.remove("active");
+            button.changeText(speed);
+            return;
+        }
+        speed += 0.25;
+        button.classList.add("active");
+        button.changeText(speed);
+        storageSpeed.set(speed);
+    })
+
+    document.addEventListener("click", () => {
+        const audios = document.querySelectorAll("audio");
+        audios.forEach((audio) => {
+            audio.playbackRate = speed;
         })
-
-        document.addEventListener("click", () => {
-            const audios = document.querySelectorAll("audio");
-            audios.forEach((audio) => {
-                audio.playbackRate = speed;
-            })
-        })
-
-    }
+    })
 }, 1000)
+
+function speedText(speed) {
+    return `${speed}x`;
+}
+
+function createButton (speed) {
+    const button = document.createElement("button");
+    button.changeText = (s) => {button.innerHTML = speedText(s)};
+    button.classList.add("twoTimesButton");
+    if(speed > 1) button.classList.add("active");
+    button.changeText(speed);
+    return button;
+}
